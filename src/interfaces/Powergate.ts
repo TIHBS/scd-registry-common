b4162a2dff89ae37e1b8bb1e2d00f5e8abe34752
GarondEisenfaust <chr.schr@web.de>
2022-05-10 16:52:32 +0200

    adds StorageJobInfo

--- src/interfaces/Powergate.ts
+++ src/interfaces/Powergate.ts
@@ -10,3 +10,18 @@ export interface StagingResult {
 export interface ApplyingResult {
   jobId: string;
 }
+
+export interface JobStatusMap {
+  JOB_STATUS_UNSPECIFIED: 0;
+  JOB_STATUS_QUEUED: 1;
+  JOB_STATUS_EXECUTING: 2;
+  JOB_STATUS_FAILED: 3;
+  JOB_STATUS_CANCELED: 4;
+  JOB_STATUS_SUCCESS: 5;
+}
+
+export interface StorageJobInfo {
+  status: JobStatusMap[keyof JobStatusMap];
+  errorCause: string;
+  createdAt: number;
+}
